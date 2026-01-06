1d2c821d03c3f98d2800e39741239279a811c848
Gabor Bonyai <gabor.bonyai@mindflytech.com>
2026-01-06 15:34:58 +0100

    Initial commit 2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "azurerm" {
-    storage_account_name = "desouza"
-    container_name       = "terraform-state"
-    key                  = "spring-react-azure-app-service.tfstate"
-    resource_group_name  = "common"
-  }
+  backend "azurerm" {}
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ provider "azurerm" {
   features {}
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
+  use_cli = false
 }
 
 module "app" {
