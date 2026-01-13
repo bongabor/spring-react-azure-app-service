cf1c553f17dbcffcd3eafd70edac9170980b6318
Gabor Bonyai <gabor.bonyai@mindflytech.com>
2026-01-13 12:10:48 +0100

    Try

--- ui/src/config.js
+++ ui/src/config.js
@@ -2,6 +2,6 @@ export const API_URL = () => {
     if (process.env.REACT_APP_ENVIRONMENT === 'local') 
         return 'http://localhost:8080';
     else if (process.env.REACT_APP_ENVIRONMENT === 'development')
-        return 'https://springreact-nonprod.azurewebsites.net'
-    else return 'https://springreact-nonprod.azurewebsites.net' // same as nonprod for now
+        return 'https://mcppoc-bff-dev.azurewebsites.net'
+    else return 'https://mcppoc-bff-dev.azurewebsites.net' // same as nonprod for now
 };
