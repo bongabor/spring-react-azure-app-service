8b4bcefb5d5b1030fc7f7a4286d582bbae9bc671
Gabor Bonyai <gabor.bonyai@mindflytech.com>
2026-01-13 11:06:45 +0100

    Try

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,13 +1,10 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.57"
+      version = "~> 4.57"  # or the latest 4.x you prefer
     }
   }
-  required_version = ">= 1.5.0"
-}
-
-provider "azurerm" {
-  features {}
 }
